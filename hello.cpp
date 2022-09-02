abfd99f750e3d2c47980fec1980d6019964af38f
osboxes.org <sooryaprakash102000@gmail.com>
2022-09-02 03:02:33 -0400

    first_merge_attempt

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,10 @@
 
 int main()
 {
-int a=10;
+int a=10,b=2;
 std::cout<<"A= "<<a<<std::endl;
+std::cout<<"B= "<<b<<std::endl;
+std::cout<<"Sum= "<<a+b<<std::endl;
 std::cout<<"hello world\n";
  std::cout<<"Edited in git-online\n";
 return 0;
